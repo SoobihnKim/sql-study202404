68605056352a3ddb19f77dce79e481ca7bbce03f
soo <ksbsj92@naver.com>
2024-04-30 12:54:44 +0900

    practice 2 0430

--- 실습2 hr.sql
+++ 실습2 hr.sql
@@ -13,6 +13,9 @@ SELECT
   TO_CHAR(hire_date, 'YYYY')
 FROM employees;
 
+SELECT
+EXTRACT(YEAR FROM hire_date)
+FROM employees;
 -- 4. employees 테이블에서 각 사원의 hire_date의 월과 일을 연결하여 출력하세요. 예: MM-DD 형식.
 SELECT
   TO_CHAR(hire_date, 'MM-DD')
@@ -24,13 +27,13 @@ SELECT
 FROM departments;
 
 -- 6. employees 테이블에서 manager_id가 NULL인 사원의 이름과 직책을 출력하세요.
-SELECT
-  NVL(manager_id, last_name),
-  NVL(manager_id, job_id)
-FROM employees;
+SELECT first_name, job_id
+FROM employees
+WHERE manager_id IS NULL ;
 
 -- 7. jobs 테이블에서 job_title의 길이를 계산하여 출력하세요.
 SELECT
+ job_title,
  LENGTH(job_title)
 FROM jobs;
 
@@ -42,9 +45,14 @@ SELECT
 FROM employees;
 
 -- 9. employees 테이블에서 각 사원의 성의 첫 글자와 이름을 연결하여 출력하세요.
-SELECT
-  CONCAT(SUBSTR(first_name, 1,1),last_name)
+SELECT 
+ CONCAT(SUBSTR(last_name, 1, 1), first_name) 
 FROM employees;
+
+SELECT 
+SUBSTR(last_name, 1, 1) || first_name 
+FROM employees;
+
 
 -- 10. locations 테이블에서 city의 이름을 역으로 출력하세요. 
 SELECT
@@ -56,19 +64,25 @@ SELECT
   ROUND(salary, -2)
 FROM employees;
 
+SELECT 
+CEIL(salary/100)*100, salary 
+FROM employees;
+
+
 -- 12. employees 테이블에서 각 사원의 commission_pct가 NULL일 경우, 0으로 표시하여 출력하세요.
 SELECT
+  first_name
   NVL(commission_pct, 0)
 FROM employees;
 
 -- 13. employees 테이블에서 각 사원의 이름의 첫 글자와 성의 첫 글자를 연결하여 출력하세요.
 SELECT
-  CONCAT(SUBSTR(last_name, 1,1), SUBSTR(first_name, 1,1))
+  CONCAT(SUBSTR(first_name, 1, 1), SUBSTR(last_name, 1, 1))
 FROM employees;
 
 -- 14. employees 테이블에서 각 사원의 salary에 대해 10% 인상한 금액을 계산하여 출력하세요.
 SELECT
-  
+ salary * 1.10 AS new_salary  
 FROM employees;
 
 -- 15. countries 테이블에서 각 국가의 이름을 세 글자로 줄여서 출력하세요.
@@ -78,15 +92,16 @@ FROM countries;
 
 -- 16. employees 테이블에서 각 사원의 last_name의 두 번째 글자를 '*'로 변경하여 출력하세요.
 SELECT
- LPAD(last_name, 2,'*')
+ SUBSTR(last_name, 1, 1) || '*' || SUBSTR(last_name, 3)
 FROM employees;
 
 -- 17. employees 테이블에서 job_id가 it_prog인 사원의 first_name과 salary를 출력하세요.
 --  조건1 ) 비교하기 위한 값은 소문자로 입력할 것!
 --  조건2 ) 이름은 앞 3글자까지만 출력하고 나머지는 *로 마킹할 것. 
 --         이 열의 별칭은 name입니다.
-SELECT
- RPAD(first_name, 3, '*')
- 
+SELECT 
+    RPAD(SUBSTR(first_name, 1, 3), LENGTH(first_name), '*') AS name,
+    salary  
 FROM employees
-WHERE job_id = 'it_prog';
+WHERE LOWER(job_id) = 'it_prog'
+;
