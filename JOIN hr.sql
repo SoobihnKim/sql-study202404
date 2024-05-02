be87a5b76977f4d4ad27c224d75a5066bbae4c87
soo <ksbsj92@naver.com>
2024-05-02 12:20:09 +0900

    Join

---
+++ JOIN hr.sql
@@ -0,0 +1,146 @@
+
+-- 사원의 first_name, employee_id, department_id, dept_name
+-- null이면 INNER JOIN에서 조회안됨
+SELECT
+    E.employee_id,
+    E.first_name,
+    E.department_id,
+    D.department_name
+FROM employees E
+INNER JOIN departments D
+ON E.department_id = D.department_id
+ORDER BY E.employee_id
+;
+
+-- NATURAL JOIN, INNER JOIN 항상 같은 결과를 가져오지 않음
+-- 여기에서는 공통컬럼이 두개여서 두개 적용됨 (department_id, manager_id)
+-- 이름이 같으면 모두 걸림
+SELECT
+    E.employee_id,
+    E.first_name,
+    department_id,
+    D.department_name
+FROM employees E
+NATURAL JOIN departments D
+ORDER BY E.employee_id
+;
+
+SELECT
+    E.employee_id,
+    E.first_name,
+    E.department_id,
+    D.department_name
+FROM employees E
+INNER JOIN departments D
+ON E.department_id = D.department_id
+ AND E.manager_id = D.manager_id
+ORDER BY E.employee_id
+;
+
+SELECT
+    E.employee_id,
+    E.first_name,
+    department_id,
+    D.department_name
+FROM employees E
+INNER JOIN departments D
+USING (department_id)
+ORDER BY E.employee_id
+;
+
+SELECT
+    E.employee_id,
+    E.first_name,
+    department_id,
+    D.department_name
+FROM employees E
+INNER JOIN departments D
+USING (department_id, manager_id)
+ORDER BY E.employee_id
+;
+
+-- 실습
+-- 1. employees테이블과 departments테이블을 inner join하여
+--    사번, first_name, last_name, department_id, department_name을 조회하세요.
+SELECT
+    E.employee_id,
+    E.first_name,
+    E.last_name,
+    E.department_id,
+    D.department_name
+FROM employees E
+INNER JOIN departments D
+ON E.department_id = D.department_id
+;
+
+-- 2. employees테이블과 departments테이블을 natural join하여
+--    사번, first_name, last_name, department_id, department_name을 조회하세요.
+SELECT
+    E.employee_id,
+    E.first_name,
+    E.last_name,
+    department_id,
+    D.department_name
+FROM employees E
+NATURAL JOIN departments D
+;
+
+-- 3. employees테이블과 departments테이블을 using절을 사용하여
+--    사번, first_name, last_name, department_id, department_name을 조회하세요.
+SELECT
+    E.employee_id,
+    E.first_name,
+    E.last_name,
+    department_id,
+    D.department_name
+FROM employees E
+INNER JOIN departments D
+USING (department_id)
+ORDER BY E.employee_id
+;
+--  4. employees테이블과 departments테이블과 locations 테이블을 
+--    join하여 employee_id, first_name, department_name, city를 조회하세요.
+SELECT
+    E.employee_id,
+    E.first_name,
+    D.department_name,
+    L.city
+FROM employees E
+INNER JOIN departments D
+ON E.department_id = D.department_id
+JOIN locations L
+ON d.location_id = l.location_id
+;
+
+-- 5. employees 테이블과 jobs 테이블을 INNER JOIN하여 
+-- 사원의 first_name, last_name, job_title을 조회하세요.
+SELECT
+    E.first_name,
+    E.last_name,
+    J.job_title
+FROM employees E
+INNER JOIN jobs J
+ON E.job_id = J.job_id
+;
+-- 6. employees 테이블과 departments 테이블을 INNER JOIN하여 
+-- 부서명과 각 부서의 최대 급여값을 조회하세요.
+SELECT
+    D.department_name,
+    MAX(E.salary)
+FROM employees E
+INNER JOIN departments D
+ON e.department_id = d.department_id
+GROUP BY department_name
+;
+-- 7. employees 테이블과 jobs 테이블을 INNER JOIN하여 
+--    직무별 평균 급여와 직무 타이틀을 조회하세요.
+SELECT
+    j.job_title,
+    AVG(salary) "Average salary"
+FROM employees E
+INNER JOIN jobs J
+ON E.job_id = J.job_id
+GROUP BY job_title
+;
+
+
